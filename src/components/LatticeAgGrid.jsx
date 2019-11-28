6cff763bb1306d526a1e3bccc5d56b4d20402794
Shivani Sharma <sharma.14shivani@gmail.com>
2019-11-28 11:28:31 +0530

    LJS-10 removed commented code

--- src/components/LatticeAgGrid.jsx
+++ src/components/LatticeAgGrid.jsx
@@ -4,22 +4,11 @@ import { Grid } from '@material-ui/core';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 // import 'ag-grid-community/dist/styles/ag-theme-material.css';
+// import 'ag-grid-community/dist/styles/ag-theme-material-dark.css';
 import '../styles/ag-theme-material.css';
 import '../styles/ag-theme-material-dark.css';
-
-// import 'ag-grid-community/dist/styles/ag-theme-material.css';
-// import 'ag-grid-community/dist/styles/ag-theme-material-dark.css';
-
 import { withTheme } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/core/styles';
 import 'typeface-roboto';
-
-
-const styles = theme => ({
-  content: {
-    fontFamily: theme.typography.fontFamily,
-  }
-});
 
 class LatticeAgGrid extends Component {
   constructor(props) {
@@ -54,7 +43,6 @@ class LatticeAgGrid extends Component {
         }}
       >
         <AgGridReact
-            className={classes.content}
             {...this.props}
         >
         </AgGridReact>
@@ -63,16 +51,4 @@ class LatticeAgGrid extends Component {
   }
 }
 
-// export default withTheme(LatticeAgGrid);
-export default withStyles(styles, { withTheme: true })(LatticeAgGrid);
-
-// animateRows={this.props.animateRows}
-// enableSorting={this.props.enableSorting}
-// enableFilter={this.props.enableFilter}
-// enableColResize={this.props.enableColResize}
-// rowDragManaged={!this.props.showPagination}
-// rowSelection={this.props.rowSelection}
-// pagination={this.props.showPagination}
-// paginationAutoPageSize={this.props.showPagination}
-// columnDefs={this.props.columnDefs}
-// rowData={this.props.rowData}
+export default withTheme(LatticeAgGrid);
